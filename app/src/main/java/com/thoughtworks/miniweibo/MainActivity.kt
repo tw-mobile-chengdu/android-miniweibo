9cd6b4e6235b796f117de92186104a8142397198
Tang Zhen <cloudztang@gmail.com>
2019-12-24 13:26:47 +0800

    [Zhen] use NavigationUI bind the Action Bar.

--- app/src/main/java/com/thoughtworks/miniweibo/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/miniweibo/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.thoughtworks.miniweibo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController =
+            Navigation.findNavController(this, R.id.container)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController =
+            Navigation.findNavController(this, R.id.container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
