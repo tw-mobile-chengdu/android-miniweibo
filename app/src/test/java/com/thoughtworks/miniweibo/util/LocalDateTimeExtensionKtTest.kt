1b2a244249f8e284ce5cd3267c6c5bfc18d01c70
Tang Zhen <cloudztang@gmail.com>
2020-01-06 14:31:14 +0800

    [Zhen] change to use the hamcrest matcher.

--- app/src/test/java/com/thoughtworks/miniweibo/util/LocalDateTimeExtensionKtTest.kt
+++ app/src/test/java/com/thoughtworks/miniweibo/util/LocalDateTimeExtensionKtTest.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.miniweibo.util
 
-import org.junit.Assert.assertEquals
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -12,41 +13,44 @@ class LocalDateTimeExtensionKtTest {
     fun `should return minutes when less than one hour`() {
         val localDateTime = LocalDateTime.now().minus(30, ChronoUnit.MINUTES)
 
-        assertEquals("30 分钟前", localDateTime.getShortFormat())
+        assertThat<String>(localDateTime.getShortFormat(), `is`("30 分钟前"))
     }
 
     @Test
     fun `should return at the moment when less than one minute`() {
         val lessThanOneHours = LocalDateTime.now().minus(30, ChronoUnit.SECONDS)
 
-        assertEquals("刚刚", lessThanOneHours.getShortFormat())
+        assertThat<String>(lessThanOneHours.getShortFormat(), `is`("刚刚"))
     }
 
     @Test
-    fun `should return hours when date time less than one day`() {
+    fun `should return hours when   time less than one day`() {
         val localDateTime = LocalDateTime.now().minus(10, ChronoUnit.HOURS)
 
-        assertEquals("10 小时前", localDateTime.getShortFormat())
+        assertThat<String>(localDateTime.getShortFormat(), `is`("10 小时前"))
     }
 
     @Test
     fun `should return yesterday when date time less than two day`() {
         val localDateTime = LocalDateTime.now().minus(26, ChronoUnit.HOURS)
 
-        assertEquals("昨天", localDateTime.getShortFormat())
+        assertThat<String>(localDateTime.getShortFormat(), `is`("昨天"))
     }
 
     @Test
     fun `should return the day before yesterday when date time less than three day`() {
         val localDateTime = LocalDateTime.now().minus(49, ChronoUnit.HOURS)
 
-        assertEquals("前天", localDateTime.getShortFormat())
+        assertThat<String>(localDateTime.getShortFormat(), `is`("前天"))
     }
 
     @Test
     fun `should return the date info when date time more than three day`() {
         val localDateTime = LocalDateTime.now().minus(5, ChronoUnit.DAYS)
 
-        assertEquals(localDateTime.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日")), localDateTime.getShortFormat())
+        assertThat<String>(
+            localDateTime.getShortFormat(),
+            `is`(localDateTime.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日")))
+        )
     }
 }
